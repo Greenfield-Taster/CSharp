2589c97f9be06f63cdc32bc7b30b1835eb2332e3
asago <asago@DESKTOP-RE3V1LE>
2022-08-28 13:52:39 +0300

    Добавьте файлы проекта.

---
+++ BaseOfCSharp/FirstSkills/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i+=2)//вывод только четных чисел в столбик
+            {
+                Console.WriteLine("Value of i: {0}", i);
+            }
+            for (int i = 0; i < 10; i += 2)//вывод только четных чисел в рядок
+            {
+                Console.Write("{0} , ", i);
+            }
+        }
+    }
+}
